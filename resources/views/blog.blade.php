dfab2741c95451848a66af827abe6bbcb1206a9e
Steven <ststeven82@gmail.com>
2024-08-09 17:05:40 +0700

    First Commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero maiores, sint possimus nulla ipsam, impedit
+            voluptate dignissimos omnis ea nam ipsa aliquam itaque quos reiciendis aliquid rem consequuntur recusandae
+            dicta.</p>
+    </article>
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, fugit delectus eaque sit, aut odio minima
+            ipsum vero laborum debitis maxime. Quibusdam eum possimus, eveniet veniam itaque illo iure fuga.</p>
+    </article>
+</body>
+
+</html>
